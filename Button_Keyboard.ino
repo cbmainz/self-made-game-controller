db38032b79d7388bae62af3656247b6806491630
cbmainz <github@cbmainz.de>
2017-10-12 18:46:35 +0200

    Two buttons an german translation
    Two buttons an german translation

--- Button_Keyboard.ino
+++ Button_Keyboard.ino
@@ -1,48 +1,69 @@
-#include <Bounce2.h> // to access the bounce lib we added
-#include <Keyboard.h> // access the built in keyboard library
+// Beispiel Code für einen Arduino Pro Micro und zwei Arcade-Button die beim drücken Tastaturbefehle senden.
+// Per USB an einen Computer angeschlossen, kann so ein einfacher GameController gebaut werden.
 
-#define BUTTON_PIN 3
+#include <Bounce2.h> // Bindet die Bounce2-Bibliothek ein
+#include <Keyboard.h> // Bindet die Tatstur-Bibliothek ein
 
-const char key = ' '; // going to press the space key
-Bounce button = Bounce(); // create bounce button called button
+// Definiert, an welchen digitalen Anschlüssen des Arduinos die Buttons angeschlossen sind:
+#define BUTTON_ONE 8
+#define BUTTON_TWO 9
+
+// Definiert, welcher Buchstabe beim drücken geschrieben wird:
+const char key_one = 'x'; // hier x für button_one
+const char key_two = ' '; // hier die LEERTASTE für button_two
+// char key_two = KEY_RETURN; // hier RETURN (mehr Infos: https://www.arduino.cc/en/Reference/KeyboardModifiers)
+
+// Definiert die beiden Buttons als Bounce Buttons
+Bounce button_one = Bounce();
+Bounce button_two = Bounce();
 
 void setup() {
-  // put your setup code here, to run once:
+  // Setup Code, wierd beim Start einmal aufgerufen:
 
-  // use arduino as a keyboard
+  // Richtet den Arduino als Tastatur ein:
   Keyboard.begin();
 
-  // set button pin to be an input
-  // activate pull up resistor
-  pinMode( BUTTON_PIN , INPUT_PULLUP);
+  // Richtet die digitalen Anschlüssen, an denen die Buttons angeschlossen sind, als Input einund aktiviert den Pullup-Wiederstand
+  pinMode( BUTTON_ONE , INPUT_PULLUP);
+  pinMode( BUTTON_TWO , INPUT_PULLUP);
 
-  // setup the button
-  button.attach( BUTTON_PIN );
-  button.interval(5);
+  // Richtet die Buttons ein:
+  button_one.attach( BUTTON_ONE );
+  button_two.attach( BUTTON_TWO );
+  button_one.interval(5);
+  button_two.interval(5);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Haupteil des Programmes, dieser Code wird immer wiederholt:
 
-  // button.update is true if button state has changed.
-  if (button.update()) {
+  // Wenn-Abfrage für den ersten Button:
 
-    // if button went from 1 to 0 - ie. was pressed
-    if(button.fell()) {
-      Keyboard.press(key); // press key down
+  // button_one.update ist Wahr, wenn sich der Staus von button_one ändert.
+  if (button_one.update()) {
+
+    // Wenn sich der Wert des Buttons von 1 auf 0 ändert - bspw., wenn er gedrückt wurde
+    if(button_one.fell()) {
+      Keyboard.press(key_one);
     }
-    // if button went went from 0 to 1 - ie. was released
-    else if (button.rose()) {
-      Keyboard.release(key); // release key
+    // Wenn sich der Wert des Buttons von 0 auf 1 ändert - bspw., wenn er wieder losgelassen wurde
+    else if (button_one.rose()) {
+      Keyboard.release(key_one);
     }
   }
+  // Wenn-Abfrage für den zweiten Button:
   
-  // OR - for hold down key, but then kill down the upper code
+  // button_two.update ist Wahr, wenn sich der Staus von button_two ändert.
+  if (button_two.update()) {
 
-  // if(button.update()) {
-    // if(button.fell()) {
-      // Keyboard.write(key); // send 1 keypress |
-    
-   // }
+    // Wenn sich der Wert des Buttons von 1 auf 0 ändert - bspw., wenn er gedrückt wurde
+    if(button_two.fell()) {
+      Keyboard.press(key_two);
+    }
+    // Wenn sich der Wert des Buttons von 0 auf 1 ändert - bspw., wenn er wieder losgelassen wurde
+    else if (button_two.rose()) {
+      Keyboard.release(key_two);
+    }
   }
+ }
  
